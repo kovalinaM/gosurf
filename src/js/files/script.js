15a5617d9a697c5ddaf2177bc56e4ebed745db38
Marina Kovaleva <sunshining1106@mail.ru>
2023-07-12 18:34:54 +0300

    add surf-slider

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,3 @@
-// Подключение функционала "Чертогов Фрилансера"
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
@@ -22,4 +21,12 @@ $(function(){
         slidesToScroll: 1,
         asNavFor: '.header__slider'
     });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider__arrows slider__arrows_left" src="img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider__arrows slider__arrows_right" src="img/arrows-right.svg" alt="">',
+    });
+
 });
